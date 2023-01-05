0c5720d92a5a11c6adb2962b96a8d4727eeb6ac9
Kiren <kirenraj21m@forwardschool.edu.my>
2023-01-05 16:17:35 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+// get route from the global variable
+$path = $_SERVER["REQUEST_URI"];
+//remove beginning & ending trailing slash
+$path = trim( $path, '/');
+//remove all the URL parameters that starts with ?
+$path = parse_url($path, PHP_URL_PATH);
+
+switch($path){
+    case 'login':
+        require "pages/login.php";
+        break;
+    case 'signup':
+        require "pages/signup.php";
+        break;  
+    case 'dashboard':
+        require "pages/dashboard.php";
+        break;  
+    case 'post':
+        require "pages/post.php";
+        break;
+    case 'manage-posts':
+        require "pages/manage-posts.php";
+        break;
+    case 'manage-posts-add':
+        require "pages/manage-posts-add.php";
+        break;
+    case 'manage-posts-edit':
+        require "pages/manage-posts-edit.php";
+        break;
+    case 'manage-users':
+        require "pages/manage-users.php";
+        break;
+    case 'manage-users-add':
+        require "pages/manage-users-add.php";
+        break;
+    case 'manage-users-edit':
+        require "pages/manage-users-edit.php";
+        break;
+    case 'logout':
+        require "pages/logout.php";
+        break; 
+    default:
+        require "pages/home.php";
+        break; 
+     
+
+}
